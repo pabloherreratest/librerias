b41a0905f803aca1f518f19182272bb91e13a1de
Pablo Herrera <pabloherreratest@gmail.com>
2025-03-09 08:48:27 -0500

    ejemplo POST

--- cap04/ejemploAxios.js
+++ cap04/ejemploAxios.js
@@ -1,64 +1,76 @@
 import axios from 'axios';
 
 async function obtenerUsuariosUsandoFetch() {
+    
     try {
-        const response = await fetch('https://jsonplaceholder.typicode.com/users2');
+        const response = await fetch('https://jsonplaceholder.typicode.com/usersXYZ');
         const json = await response.json();
-        if(!response.ok) {
-            throw new Error(`Error HTTP: ${response.status} - ${response.statusText}`);
+        if (!response.ok) {
+            throw new Error('Mi control: ' + response.status + ' - ' + response.statusText);
         }
         return json;
-      } catch (error) {
-        console.error("Error al obtener la tarea:", error.message);
+    } catch (error) {
+        console.error('Error (Fetch):', error);
         return [];
-      }
+    }
+      
 }
 
 async function obtenerUsuarios() {
+    
     try {
-        const respuesta = await axios.get('https://jsonplaceholder.typicode.com/users2');
-        return respuesta.data;
+        const response = await axios.get('https://jsonplaceholder.typicode.com/usersXYZ');           
+        return response.data;
     } catch (error) {
-        console.error('Error al obtener los datos:', error);
+        console.error('Error (Axios):', error);
+        return [];
     }
+      
 }
 
 async function obtenerUsuarioPorID(id) {
+    
     try {
-        const respuesta = await axios.get('https://jsonplaceholder.typicode.com/users/' + id);
-        return respuesta.data;
+        const response = await axios.get('https://jsonplaceholder.typicode.com/users/'+id);           
+        return response.data;
     } catch (error) {
-        console.error('Error al obtener el usuario:', error);
+        console.error('Error (Axios):', error);
+        return [];
     }
+      
 }
 
-async function crearPost(userId, title, body) {
+    async function crearComentario(postId, name, email, body) {
 
-    const post = {
-        userId: userId,
-        id: 101,
-        title: title,
-        body: body
+        const comentario = {
+            postId: postId,
+            name: name,
+            email: email,
+            body: body
+        };
+        
+        try {
+            const response = await axios.post('https://jsonplaceholder.typicode.com/comments', comentario);           
+            return response.data;
+        } catch (error) {
+            console.error('Error (Axios):', error);
+            return [];
+        }
+        
     }
 
-    try {
-        const respuesta = await axios.post('https://jsonplaceholder.typicode.com/posts',post);
-        return respuesta.data;
-    } catch (error) {
-        console.error('Error al obtener el usuario:', error);
-    }
-}
-
+/*
 const listaDeUsuarios01  = await obtenerUsuariosUsandoFetch();
 console.log(listaDeUsuarios01); // 📌 Mostrar los datos de la API
 
-const listaDeUsuarios  = await obtenerUsuarios();
-console.log(listaDeUsuarios); // 📌 Mostrar los datos de la API
-
-const usuario = await obtenerUsuarioPorID(3);
-console.log(usuario); // 📌 Mostrar los datos de un usuario
-console.log("username = ", usuario.username); // 📌 Mostrar el nombre de usuario
+const listaDeUsuarios02  = await obtenerUsuarios();
+console.log(listaDeUsuarios02); // 📌 Mostrar los datos de la API
 
 
-const nuevoPost = await crearPost(1, 'Ejemplo Axios POST', 'Este es un ejemplo de prueba de POST');
-console.log(nuevoPost); // 📌 Mostrar los datos del nuevo post
+const usuario  = await obtenerUsuarioPorID(5);
+console.log(usuario.name); // 📌 Mostrar los datos de la API
+console.log(usuario.email);
+*/
+
+const comentario  = await crearComentario(1, 'Mi comentario', 'pablo@gmail.com', 'Hola mundo, este es mi primer comentario');
+console.log(comentario); // 📌 Mostrar los datos de la API
